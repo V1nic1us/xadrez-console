04f718b462044102dfb403ae7bf01d187837788e
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-10 07:58:32 -0400

    feat: add board class

---
+++ xadrez-console/tabuleiro/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    internal class Board
+    {
+        public int Lines { get; set; }
+        public int Columns { get; set; }
+        private Part[,] Parts;
+
+        public Board(int lines, int columns)
+        {
+            Lines = lines;
+            Columns = columns;
+            Parts = new Part[lines, columns];
+        }
+    }
+}
