e5d168c8f82b8fb04890682e03901c2cac1c1336
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-10 07:59:17 -0400

    feat: add part class

---
+++ xadrez-console/tabuleiro/Part.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    internal class Part
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int AmtMovements { get; protected set; }
+        public Board Board { get; protected set; }
+
+        public Part(Position position,Board board, Color color)
+        {
+            Position = null;
+            Color = color;
+            Board = board;
+            AmtMovements = 0;
+        }
+    }
+}
