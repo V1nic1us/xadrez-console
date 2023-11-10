74cdc39e6ab3a2567b60f0f17e018113fcb7234d
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-10 07:59:03 -0400

    feat: add color class

---
+++ xadrez-console/tabuleiro/Color.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    internal enum Color
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
