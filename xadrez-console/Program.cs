046d15b7ac51957249daa6b3d50e2c8e72a73213
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-10 08:36:22 -0400

    feat: test the board view

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using xadrez_console.tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board(8, 8);
+
+            Screen.PrintBoard(board);
+
+            Console.ReadLine();
+        }
+    }
+}
+
