739ba46b2adf6156cee1ee2c9c1ea2bbe20298d1
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-10 23:26:30 -0400

    feat: added test pieces to the board

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using xadrez_console.tabuleiro;
+using xadrez_console.Parts;
 
 namespace xadrez_console
 {
@@ -8,6 +9,14 @@ namespace xadrez_console
         static void Main(string[] args)
         {
             Board board = new Board(8, 8);
+
+            board.PutPart(new Tower(board, Color.Black), new Position(0, 0));
+            board.PutPart(new Tower(board, Color.Black), new Position(1, 3));
+            board.PutPart(new King(board, Color.Black), new Position(0, 2));
+            board.PutPart(new Tower(board, Color.White), new Position(3, 5));
+            board.PutPart(new Tower(board, Color.White), new Position(4, 5));
+            board.PutPart(new King(board, Color.White), new Position(3, 4));
+            board.PutPart(new Tower(board, Color.White), new Position(3, 5));
 
             Screen.PrintBoard(board);
 
