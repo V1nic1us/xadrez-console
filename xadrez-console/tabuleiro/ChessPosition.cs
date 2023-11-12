1421fb838b92cdcba4ce74de740497874e80310a
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-11 23:52:15 -0400

    feat: added class to display position in chess style

---
+++ xadrez-console/tabuleiro/ChessPosition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    internal class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Row { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Column - 'a'); // 'a' - 'a' = 0
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Row; // "" + 1 + 2 = "12
+        }
+    }
+}
