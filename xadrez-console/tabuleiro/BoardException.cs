93ec3dcfbf1ab5f2f507d432108a3c9cf8cb1254
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-11 23:40:47 -0400

    feat: creation of error class with custom message

---
+++ xadrez-console/tabuleiro/BoardException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    internal class BoardException : Exception
+    {
+        public BoardException()
+        {
+        }
+
+        public BoardException(string message) : base(message)
+        {
+        }
+
+        public BoardException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
