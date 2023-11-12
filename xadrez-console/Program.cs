d38703942b30616da8881834f602a78de9ada2a7
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-11 23:44:07 -0400

    feat: adicionado captura e exibição do erro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,19 +8,27 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);
+            try
+            {
+                Board board = new Board(8, 8);
 
-            board.PutPart(new Tower(board, Color.Black), new Position(0, 0));
-            board.PutPart(new Tower(board, Color.Black), new Position(1, 3));
-            board.PutPart(new King(board, Color.Black), new Position(0, 2));
-            board.PutPart(new Tower(board, Color.White), new Position(3, 5));
-            board.PutPart(new Tower(board, Color.White), new Position(4, 5));
-            board.PutPart(new King(board, Color.White), new Position(3, 4));
-            board.PutPart(new Tower(board, Color.White), new Position(3, 5));
+                board.PutPart(new Tower(board, Color.Black), new Position(0, 0));
+                board.PutPart(new Tower(board, Color.Black), new Position(1, 3));
+                board.PutPart(new King(board, Color.Black), new Position(0, 2));
+                board.PutPart(new Tower(board, Color.White), new Position(3, 5));
+                board.PutPart(new Tower(board, Color.White), new Position(4, 5));
+                board.PutPart(new King(board, Color.White), new Position(3, 4));
+                board.PutPart(new Tower(board, Color.White), new Position(3, 7));
 
-            Screen.PrintBoard(board);
+                Screen.PrintBoard(board);
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
