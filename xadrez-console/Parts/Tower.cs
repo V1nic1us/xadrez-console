8236aab7f77fb9a417da049f1b180c6775d7ef56
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-13 23:54:36 -0400

    feat: add the methods of the tower can move and tower movements

--- xadrez-console/Parts/Tower.cs
+++ xadrez-console/Parts/Tower.cs
@@ -15,5 +15,63 @@ namespace xadrez_console.Parts
         {
             return "T";
         }
+
+        private bool CanMove(Position position)
+        {
+            Part p = Board.Part(position);
+            return p == null || p.Color != Color;
+        }
+
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+
+            Position pos = new Position(0, 0);
+
+            pos.SetValues(Position.Row - 1, Position.Column);
+            while (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+                if (Board.Part(pos) != null && Board.Part(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Row = pos.Row - 1;
+            }
+
+            pos.SetValues(Position.Row + 1, Position.Column);
+            while (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+                if (Board.Part(pos) != null && Board.Part(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Row = pos.Row + 1;
+            }
+
+            pos.SetValues(Position.Row, Position.Column + 1);
+            while (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+                if (Board.Part(pos) != null && Board.Part(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Column = pos.Column + 1;
+            }
+
+            pos.SetValues(Position.Row, Position.Column - 1);
+            while (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+                if (Board.Part(pos) != null && Board.Part(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Column = pos.Column - 1;
+            }
+            return mat;
+        }
     }
 }
