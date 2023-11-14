ba16ee6643e1df153f77e3a833750a550c6de53e
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-13 23:53:44 -0400

    feat: add king can move method and king moves

--- xadrez-console/Parts/King.cs
+++ xadrez-console/Parts/King.cs
@@ -15,5 +15,75 @@ namespace xadrez_console.Parts
         {
             return "K";
         }
+
+        private bool CanMove(Position position)
+        {
+            Part p = Board.Part(position);
+            return p == null || p.Color != Color;
+        }
+
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+
+            Position pos = new Position(0, 0);
+
+            // Above
+            pos.SetValues(Position.Row - 1, Position.Column);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+            
+            // Northeast
+            pos.SetValues(Position.Row - 1, Position.Column + 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            // Right
+            pos.SetValues(Position.Row, Position.Column + 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            // Southeast
+            pos.SetValues(Position.Row + 1, Position.Column + 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            // Below
+            pos.SetValues(Position.Row + 1, Position.Column);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            // Southwest
+            pos.SetValues(Position.Row + 1, Position.Column - 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            // Left
+            pos.SetValues(Position.Row, Position.Column - 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+
+            // Northwest
+            pos.SetValues(Position.Row - 1, Position.Column - 1);
+            if (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+            }
+            return mat;
+        }
     }
 }
