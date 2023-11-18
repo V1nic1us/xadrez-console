fd0497a74e7db78c46a650838c4f28bfc7cb2cbf
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-18 11:54:47 -0400

    fear: add queen piece moves

--- xadrez-console/Parts/Queen.cs
+++ xadrez-console/Parts/Queen.cs
@@ -7,7 +7,7 @@ using xadrez_console.tabuleiro;
 
 namespace xadrez_console.Parts
 {
-    internal class Queen: Part
+    internal class Queen : Part
     {
         public Queen(Board board, Color color) : base(board, color) { }
 
@@ -24,7 +24,35 @@ namespace xadrez_console.Parts
 
         public override bool[,] PossibleMovements()
         {
-            throw new NotImplementedException();
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+            Position pos = new Position(0, 0);
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i != 0 || j != 0)
+                    {
+                        int row = Position.Row + i;
+                        int col = Position.Column + j;
+
+                        while (Board.ValidPosition(new Position(row, col)) && CanMove(new Position(row, col)))
+                        {
+                            mat[row, col] = true;
+
+                            if (Board.Part(new Position(row, col)) != null && Board.Part(new Position(row, col)).Color != Color)
+                            {
+                                break;
+                            }
+
+                            row += i;
+                            col += j;
+                        }
+                    }
+                }
+            }
+
+            return mat;
         }
     }
 }
