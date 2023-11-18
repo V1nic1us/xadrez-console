9246c56e538dd94aa85996b0042850a2e0351c79
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-18 11:58:22 -0400

    feat: add movements from the knight piece

--- xadrez-console/Parts/Knight.cs
+++ xadrez-console/Parts/Knight.cs
@@ -7,7 +7,7 @@ using xadrez_console.tabuleiro;
 
 namespace xadrez_console.Parts
 {
-    internal class Knight: Part
+    internal class Knight : Part
     {
         public Knight(Board board, Color color) : base(board, color) { }
 
@@ -24,7 +24,25 @@ namespace xadrez_console.Parts
 
         public override bool[,] PossibleMovements()
         {
-            throw new NotImplementedException();
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+
+            int[] rowMoves = { -2, -1, 1, 2, 2, 1, -1, -2 };
+            int[] colMoves = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                int newRow = Position.Row + rowMoves[i];
+                int newCol = Position.Column + colMoves[i];
+
+                Position pos = new Position(newRow, newCol);
+
+                if (Board.ValidPosition(pos) && CanMove(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+            }
+
+            return mat;
         }
     }
 }
