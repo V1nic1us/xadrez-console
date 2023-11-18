e4cc1ea47a0a1da4d80a212740f721f0530b865d
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-18 12:20:58 -0400

    feat: add pawn piece moves

--- xadrez-console/Parts/Pawn.cs
+++ xadrez-console/Parts/Pawn.cs
@@ -22,9 +22,72 @@ namespace xadrez_console.Parts
             return p == null || p.Color != Color;
         }
 
+        private bool existEnemy(Position position)
+        {
+            Part p = Board.Part(position);
+            return p != null && p.Color != Color;
+        }
+
         public override bool[,] PossibleMovements()
         {
-            throw new NotImplementedException();
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+            Position pos = new Position(0, 0);
+
+            if (Color == Color.White)
+            {
+                pos.SetValues(Position.Row - 1, Position.Column);
+                if (Board.ValidPosition(pos) && CanMove(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.SetValues(Position.Row - 2, Position.Column);
+                Position p2 = new Position(Position.Row - 1, Position.Column);
+                if (Board.ValidPosition(p2) && CanMove(p2) && Board.ValidPosition(pos) && CanMove(pos) && AmtMovements == 0)
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+                    pos.SetValues(Position.Row - 1, Position.Column - 1);
+                    if (Board.ValidPosition(pos) && existEnemy(pos))
+                    {
+                        mat[pos.Row, pos.Column] = true;
+                    }
+
+                    pos.SetValues(Position.Row - 1, Position.Column + 1);
+                    if (Board.ValidPosition(pos) && existEnemy(pos))
+                    {
+                        mat[pos.Row, pos.Column] = true;
+                    }
+                
+            }
+            else
+            {
+                pos.SetValues(Position.Row + 1, Position.Column);
+                if (Board.ValidPosition(pos) && CanMove(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.SetValues(Position.Row + 2, Position.Column);
+                Position p2 = new Position(Position.Row + 1, Position.Column);
+                if (Board.ValidPosition(p2) && CanMove(p2) && Board.ValidPosition(pos) && CanMove(pos) && AmtMovements == 0)
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.SetValues(Position.Row + 1, Position.Column - 1);
+                if (Board.ValidPosition(pos) && existEnemy(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.SetValues(Position.Row + 1, Position.Column + 1);
+                if (Board.ValidPosition(pos) && existEnemy(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+            }
+            return mat;
         }
     }
 }
