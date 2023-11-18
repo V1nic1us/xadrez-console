e79f929897c2989e3352c392408ff1efcc8b3307
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-18 12:18:47 -0400

    feat: add bishop piece moves

--- xadrez-console/Parts/Bishop.cs
+++ xadrez-console/Parts/Bishop.cs
@@ -24,7 +24,58 @@ namespace xadrez_console.Parts
 
         public override bool[,] PossibleMovements()
         {
-            throw new NotImplementedException();
+            bool[,] mat = new bool[Board.Rows, Board.Columns];
+
+            Position pos = new Position(0, 0);
+
+            // cima esquerda
+            pos.SetValues(Position.Row - 1, Position.Column - 1);
+            while (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+                if (Board.Part(pos) != null && Board.Part(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.SetValues(pos.Row - 1, pos.Column - 1);
+            }
+
+            // cima direita
+            pos.SetValues(Position.Row - 1, Position.Column + 1);
+            while (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+                if (Board.Part(pos) != null && Board.Part(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.SetValues(pos.Row - 1, pos.Column + 1);
+            }
+
+            // baixo esquerda
+            pos.SetValues(Position.Row + 1, Position.Column - 1);
+            while (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+                if (Board.Part(pos) != null && Board.Part(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.SetValues(pos.Row + 1, pos.Column - 1);
+            }
+
+            // baixo direita
+            pos.SetValues(Position.Row + 1, Position.Column + 1);
+            while (Board.ValidPosition(pos) && CanMove(pos))
+            {
+                mat[pos.Row, pos.Column] = true;
+                if (Board.Part(pos) != null && Board.Part(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.SetValues(pos.Row + 1, pos.Column + 1);
+            }
+            return mat;
         }
     }
 }
