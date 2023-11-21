2a726cd0f93689ab20d73ca03cff6c193775994f
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-20 23:45:39 -0400

    feat: added EnPassant special move

--- xadrez-console/Parts/Pawn.cs
+++ xadrez-console/Parts/Pawn.cs
@@ -9,7 +9,11 @@ namespace xadrez_console.Parts
 {
     internal class Pawn : Part
     {
-        public Pawn(Board board, Color color) : base(board, color) { }
+        private ChessMatch Match;
+        public Pawn(Board board, Color color, ChessMatch match) : base(board, color)
+        {
+            Match = match;
+        }
 
         public override string ToString()
         {
@@ -47,18 +51,32 @@ namespace xadrez_console.Parts
                 {
                     mat[pos.Row, pos.Column] = true;
                 }
-                    pos.SetValues(Position.Row - 1, Position.Column - 1);
-                    if (Board.ValidPosition(pos) && existEnemy(pos))
-                    {
-                        mat[pos.Row, pos.Column] = true;
-                    }
 
-                    pos.SetValues(Position.Row - 1, Position.Column + 1);
-                    if (Board.ValidPosition(pos) && existEnemy(pos))
+                pos.SetValues(Position.Row - 1, Position.Column - 1);
+                if (Board.ValidPosition(pos) && existEnemy(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.SetValues(Position.Row - 1, Position.Column + 1);
+                if (Board.ValidPosition(pos) && existEnemy(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                if (Position.Row == 3)
+                {
+                    Position left = new Position(Position.Row, Position.Column - 1);
+                    if (Board.ValidPosition(left) && existEnemy(left) && Board.Part(left) == Match.EnPassantVulnerable)
                     {
-                        mat[pos.Row, pos.Column] = true;
+                        mat[left.Row - 1, left.Column] = true;
                     }
-                
+                    Position right = new Position(Position.Row, Position.Column + 1);
+                    if (Board.ValidPosition(right) && existEnemy(right) && Board.Part(right) == Match.EnPassantVulnerable)
+                    {
+                        mat[right.Row - 1, right.Column] = true;
+                    }
+                }
             }
             else
             {
@@ -85,6 +103,20 @@ namespace xadrez_console.Parts
                 if (Board.ValidPosition(pos) && existEnemy(pos))
                 {
                     mat[pos.Row, pos.Column] = true;
+                }
+
+                if (Position.Row == 4)
+                {
+                    Position left = new Position(Position.Row, Position.Column - 1);
+                    if (Board.ValidPosition(left) && existEnemy(left) && Board.Part(left) == Match.EnPassantVulnerable)
+                    {
+                        mat[left.Row + 1, left.Column] = true;
+                    }
+                    Position right = new Position(Position.Row, Position.Column + 1);
+                    if (Board.ValidPosition(right) && existEnemy(right) && Board.Part(right) == Match.EnPassantVulnerable)
+                    {
+                        mat[right.Row + 1, right.Column] = true;
+                    }
                 }
             }
             return mat;
